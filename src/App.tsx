29f27668dcdf14761389ebfce09effeaa3324187
Larissa Araujo <larissa.jvsa@gmail.com>
2023-02-07 21:42:40 -0300

    feat: :memo: corrigindo diretórios e criando página de erro

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Blog from './pages/Blog'
 import Contact from './pages/Contact'
+import Error from './pages/Error'
 import Home from './pages/Home'
 
 function App () {
@@ -11,6 +12,7 @@ function App () {
         <Route path='/' element={<Home />} />
         <Route path='/blog' element={<Blog />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<Error />} />
       </Routes>
     </BrowserRouter>
   )
